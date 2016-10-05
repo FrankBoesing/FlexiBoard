a2ad3671c1fae0847628593d640e39df2b15777f
Frank Bösing <f.boesing@gmx.de>
2016-10-05 18:24:36 +0200

    Update Example

--- src/examples/FlexiBoard_TalkToESP8266/FlexiBoard_TalkToESP8266.ino
+++ src/examples/FlexiBoard_TalkToESP8266/FlexiBoard_TalkToESP8266.ino
@@ -1,16 +1,23 @@
-//Flexiboard ESP8266 - Talk to ESP with AT-Firmware
+//Flexiboard ESP8266 - Talk To ESP8266 (AT Firmware)
 //(c) Frank Bösing, 2016
 
-#define WLAN_SERIAL     Serial1
-#define WLAN_SERIAL_BAUD_INIT 74880
-#define WLAN_SERIAL_BAUD 115200
-#define WLAN_RESET      24
-#define WLAN_ENABLE     16  //CH_PD
-#define WLAN_CTS        20  //GPIO15
-#define WLAN_RTS        2   //GPIO13
-#define WLAN_GPIO0      5   //Shared with IR_SND !
-#define WLAN_GPIO13     WLAN_RTS
-#define WLAN_GPIO15     WLAN_CTS
+#include <SPI.h>
+#include <ILI9341_t3.h>
+#include <XPT2046_Touchscreen.h>
+#include <FlexiBoard.h>
+
+
+void proxy() {
+  
+  while (WLAN_SERIAL.available()) {
+    Serial.write( WLAN_SERIAL.read() );
+  }
+
+  while (Serial.available()) {
+    WLAN_SERIAL.write(Serial.read());
+  }
+  
+}
 
 void setup(void) {
   WLAN_SERIAL.begin(WLAN_SERIAL_BAUD);
@@ -27,29 +34,35 @@ void setup(void) {
     ;
   }
 
-  //Reset ESP
+  //Reset ESP into FLASHING-MODE:
   digitalWrite(WLAN_RESET, LOW);
   delay(20);
   digitalWrite(WLAN_ENABLE, HIGH);
   digitalWrite(WLAN_RESET, HIGH);
 
   //The ESP - Bootloader waits for  HIGH on WLAN_GPIO0
-  digitalWriteFast(WLAN_GPIO0, HIGH);
+  digitalWrite(WLAN_GPIO0, HIGH);
   delay(200);
   //After that, we don't need it anymore:
   pinMode(WLAN_GPIO0, INPUT);
 
+
+  delay(250);
+  WLAN_SERIAL.printf("ATE1\r\n");
+  delay(300);
+  WLAN_SERIAL.printf("AT+GMR\r\n");
+
+  t=millis();
+  while (t - millis() < 1500) {
+    proxy();
+  }
+
+  
+  WLAN_SERIAL.printf("AT+CWLAP\r\n");  
+  
 }
 
 
 void loop() {
-
-  if (WLAN_SERIAL.available()) {
-    Serial.write( WLAN_SERIAL.read() );
-  }
-
-  if (Serial.available()) {
-    WLAN_SERIAL.write(Serial.read());
-  }
-
+  proxy();
 }
